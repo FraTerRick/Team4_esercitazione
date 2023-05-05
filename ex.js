2eceb0f4f81ba8ff12c0877dc2a888392e2859d8
MocanasuSebastian <mocanasu.sebastian02@gmail.com>
2023-05-05 15:21:29 +0200

    Sebastian

--- ex.js
+++ ex.js
@@ -9,3 +9,14 @@ let persons= [
       favouriteBook: "Harry Potter",
       petName: "Cesare",}
 ];
+let sebastian= [
+  { name: "Sebastian",
+    surname: "Mocanasu",
+    age: 21,
+    city: "Sanfre'",
+    hobby: "Kick-Box",
+    favouriteFood: "Sushi",
+    favouriteFilm: "Scary Movie",
+    favouriteBook: "Psicology",
+    petName: "Togo",}
+];
