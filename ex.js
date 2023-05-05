c710c2f5b7862d12c82a28f732f255c7a2927ae9
Francesco <francesco.lando1997@gmail.com>
2023-05-05 15:50:36 +0200

    aggiunto oggetto lando

--- ex.js
+++ ex.js
@@ -7,5 +7,15 @@ let persons= [
       favouriteFood: "pizza",
       favouriteFilm: "Iron Man",
       favouriteBook: "Harry Potter",
-      petName: "Cesare",}
+      petName: "Cesare",},
+      
+      { name: "Francesco",
+      surname: "Lando",
+      age: 26,
+      city: "Aprilia",
+      hobby: "D&D",
+      favouriteFood: "lasagna",
+      favouriteFilm: "Sicario",
+      favouriteBook: "Paradise Lost",
+      petName: "Kiro"}
 ];
