c575b92e2d3e0488e616e3a156ec2433f2b5ead5
Francesco <francesco.lando1997@gmail.com>
2023-05-05 15:53:47 +0200

    eliminato Lando

--- ex.js
+++ ex.js
@@ -9,13 +9,5 @@ let persons= [
       favouriteBook: "Harry Potter",
       petName: "Cesare",},
       
-      { name: "Francesco",
-      surname: "Lando",
-      age: 26,
-      city: "Aprilia",
-      hobby: "D&D",
-      favouriteFood: "lasagna",
-      favouriteFilm: "Sicario",
-      favouriteBook: "Paradise Lost",
-      petName: "Kiro"}
+      
 ];
